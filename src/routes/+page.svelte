e257b2c171cdaf9eb44fcd10051c330955d1af2e
leon <leon.tronic.plus@gmail.com>
2024-11-09 20:14:03 +0100

    Zeiteingaben eingestellt

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import Play from 'lucide-svelte/icons/play';
 	import RotateCcw from 'lucide-svelte/icons/rotate-ccw';
 	import Forward from 'lucide-svelte/icons/forward';
+	import Save from 'lucide-svelte/icons/save-all';
 
 	let time_work: number = 25*60;
 	let time_pause: number = 5*60;
@@ -15,6 +16,30 @@
 	const stroke: number = 20;
 	let circumference = 2 * Math.PI * radius;
 
+	function changeTime(minutes: number) {
+		const secondsToChange = minutes * 60;
+		remaining = Math.max(0, remaining + secondsToChange);
+		duration = remaining;
+	}
+
+	function saveTime() {
+		if (isWorkSession){
+			time_work = duration;
+		}
+		else {
+			time_pause = duration;
+		}
+	}
+
+	function resetTime() {
+		if (isWorkSession){
+			time_work = 25*60;
+		}
+		else {
+			time_pause = 5*60;
+		}
+	}
+	
 	function switchSession() {
 		remaining = (isWorkSession) ? time_pause : time_work;
 		duration = remaining;
@@ -70,20 +95,44 @@
 			<div class="absolute text-center">
 				<p class="font-bold text-7xl">{formatTime(remaining)}</p>
 				<div>
-					<button class="mt-8 bg-secondary-500 text-surface-100 rounded-full p-5" on:click={toggleTimer} disabled={isRunning && remaining === 0}>
+					<div class="mt-6" style="display: flex; align-items: center;">
+					<button style="font-size: 12px; font-weight: bold;" class="m-2 bg-secondary-500 rounded-full w-14 h-14 p-3" on:click={() => changeTime(-5)}>
+						- 5m
+					</button>
+					<button style="font-size: 12px; font-weight: bold;" class="m-2 bg-secondary-500 rounded-full w-14 h-14 p-3" on:click={() => changeTime(-1)}>
+						- 1m
+					</button>
+					<button class="m-2 bg-secondary-500 text-surface-100 rounded-full p-5" on:click={toggleTimer} disabled={isRunning && remaining === 0}>
 						{#if !isRunning}
 							<Play size={32}/>
 						{:else}
 							<Pause size={32}/>
 						{/if}
-					</button> 
+					</button>
+					<button style="font-size: 12px; font-weight: bold;" class="m-2 bg-secondary-500 rounded-full w-14 h-14 p-3" on:click={() => changeTime(1)}>
+						+ 1m
+					</button>
+					<button style="font-size: 12px; font-weight: bold;" class="m-2 bg-secondary-500 rounded-full w-14 h-14 p-3" on:click={() => changeTime(5)}>
+						+ 5m
+					</button>
+				</div>
 					<div class="mt-3 space-x-3">
-						<button class="bg-secondary-500 text-surface-100 rounded-full p-2" on:click={() => {switchSession();switchSession();}}>
+						<button id ="reset-btn" class="bg-secondary-500 text-surface-100 rounded-full p-2" on:click={(event) => {
+							if (event.detail === 1) {
+								switchSession();
+								switchSession();
+							} else if (event.detail === 2) {
+								resetTime();
+							}
+						}}>
 							<RotateCcw/>
 						</button> 
 						<button class="bg-secondary-500 text-surface-100 rounded-full p-2" on:click={switchSession}>
 							<Forward/>
 						</button> 
+						<button class="bg-primary-500 text-surface-100 rounded-full p-2" on:click={saveTime}>
+							<Save />
+						</button>
 					</div>
 				</div>
 			</div>
